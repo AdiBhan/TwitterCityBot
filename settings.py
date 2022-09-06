332cba1f87f9b4c26c47bdc50d9150abea201a71
Adi Bhan <96316689+AdiBhan@users.noreply.github.com>
2022-09-06 19:37:33 -0400

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,11 +1,11 @@
 
 
-CONSUMER_KEY = 'HHqfZrGqgFRHEdVDeMzxSj7fJ'
-CONSUMER_SECRET = 'MPdnbmIBmRobXRkMLmWUbcwpLqm0mOGhQxIJLtYl6zHbQMd8rx'
-ACCESS_TOKEN = '3361178997-L4PDzYI37bbv1Ka4IEBY578xX5NOohACgxpz1AH'
-ACCESS_TOKEN_SECRET = '3cXqNRf9hjnWv8Ga3MwQjjd4OCCkV5DhUMFAGVsCPj6pt'
-BEARER_TOKEN = 'AAAAAAAAAAAAAAAAAAAAAKUSgwEAAAAAQAlDtM79YGKK4QqMJbaLPa7GoPU%3DHmBrcDS2GMTb5PWKX9IxhVuk7E5s4wivqxHmDZT4svIlUTIJIk'
+CONSUMER_KEY = '*****************'
+CONSUMER_SECRET = '***********************************'
+ACCESS_TOKEN = '***********************************'
+ACCESS_TOKEN_SECRET = '***********************************'
+BEARER_TOKEN = '**********************************************************************************************'
 
 
 BASE_URL = "https://api.openweathermap.org/data/2.5/weather?"
-API_KEY = "e437bf7e9d67cc7c9869dc292a49da7f"
+API_KEY = "********************************"
